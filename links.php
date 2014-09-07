1a241f0bfe62002bcf8a66ea0733054f45080f34
RobertClarke <robert@rjfc.net>
2014-09-06 23:38:22 -0700

    Updated links

--- links.php
+++ links.php
@@ -11,32 +11,24 @@ show_header( 'Links', FALSE );
 
 <div class="links">
     <p>
-        <b><a href="#">Link Title</a></b><br>
-        Link description text.
+        <strong><a href="http://mcpestats.com/">Minecraft PE Servers</a></strong><br>
+        A list of the best Minecraft PE Servers.
     </p>
     <p>
-        <b><a href="#">Link Title</a></b><br>
-        Link description text.
+        <strong><a href="http://instantmcpe.com/">Free Minecraft PE Hosting</a></strong><br>
+        Start your own Minecraft PE server for free.
     </p>
     <p>
-        <b><a href="#">Link Title</a></b><br>
-        Link description text.
+        <strong><a href="http://instantmc.org/">Free Minecraft Server Hosting</a></strong><br>
+        Create a Minecraft PC server for free.
     </p>
     <p>
-        <b><a href="#">Link Title</a></b><br>
-        Link description text.
+        <strong><a href="http://minecraftcolorcodes.com/">Minecraft Color Codes</a></strong><br>
+        A handy reference website for the Minecraft chat color codes.
     </p>
     <p>
-        <b><a href="#">Link Title</a></b><br>
-        Link description text.
-    </p>
-    <p>
-        <b><a href="#">Link Title</a></b><br>
-        Link description text.
-    </p>
-    <p>
-        <b><a href="#">Link Title</a></b><br>
-        Link description text.
+        <strong><a href="http://minecraftpeupdate.com/">Minecraft PE Update</a></strong><br>
+        Information and updates for Minecraft PE.
     </p>
 </div>
 
