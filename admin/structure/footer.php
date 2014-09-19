2314b167a1e3dd3583e709b81ad4ea1f5359167c
Anatolie <anatoliediordita@gmail.com>
2014-09-18 21:56:07 -0400

    Deleted some unnecessary files.

--- admin/structure/footer.php
+++
@@ -1,9 +0,0 @@
-<?php global $page_sidebar; ?>
-<?php if ( $page_sidebar ) { ?>	        </div><?php } ?>
-<?php if ( $page_sidebar ) include( './structure/sidebar.php'); ?>
-        <div class="clearfix"></div>
-    </div>
-</div>
-
-</body>
-</html>
