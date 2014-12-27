6ffac34aeff403f4db98e83bc63d8326cc1e4ea6
Anatolie Diordita <contact@anatolie.me>
2014-12-27 00:13:02 -0500

    Added API V1 for testing.

---
+++ api/v1/posts.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+  * API: List Posts
+**/
+
+require_once('../../core.php');
+
+ob_start('ob_gzhandler');
+
+$posts = $db->from('content_maps')->order_by('`published` DESC')->where(['active' => 1])->fetch();
+
+// Set output array, will be encoded later.
+$output = [];
+
+require_once('../../core/htmlpurifier/HTMLPurifier.standalone.php');
+$purifier = new HTMLPurifier( HTMLPurifier_Config::createDefault() );
+
+// Primary post list.
+foreach( $posts as $i => $p ) {
+	
+	$p['auth']				= $user->info('username', $p['author']);
+	
+	$map['MapTitle']		= $p['title'];
+	$map['Description']		= $purifier->purify($p['description']);
+	
+	$map['Author']			= $p['auth'];
+	$map['AuthorUri']		= 'http://mcpehub.com/user/'.$p['auth'];
+	
+	$map['MapDownloadUri']	= $p['dl_link'];
+	$map['MapImageUri']		= 'http://cdn.mcpehub.com/uploads/700x200/maps/'.urlencode($p['images'][0]);
+	
+	$output['MapList'][] = $map;
+	
+} // End post foreach loop.
+
+echo json_encode($output);
+
+?>
