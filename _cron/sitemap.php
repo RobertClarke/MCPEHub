8f834bf737901c18d58d3de87eb42c0c19109324
Anatolie Diordita <contact@anatolie.me>
2015-05-13 21:41:33 -0400

    Fixed sitemap namespace version.

--- _cron/sitemap.php
+++ _cron/sitemap.php
@@ -5,7 +5,7 @@ require_once( '../core.php' );
 // Generate XML file.
 $xml = '<?xml version="1.0" encoding="UTF-8"?>
 
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.8">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 	<url>
 		<loc>http://mcpehub.com/</loc>
 		<changefreq>always</changefreq>
