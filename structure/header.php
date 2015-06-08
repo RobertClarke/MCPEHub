ebba24ed34fa2ce1a59f9c812baed38e6f040ade
RobertClarke <robert@rjfc.net>
2015-06-08 14:22:23 -0700

    Update header.php

--- structure/header.php
+++ structure/header.php
@@ -121,13 +121,11 @@ ga('send', 'pageview');
 
 </script>
 
-<!-- Start Alexa Certify Javascript -->
 <script type="text/javascript">
 _atrk_opts = { atrk_acct:"urwtl1agWBr1N8", domain:"mcpehub.com",dynamic: true};
 (function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://d31qbv1cthcecs.cloudfront.net/atrk.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
 </script>
 <noscript><img src="https://d5nxst8fruw4z.cloudfront.net/atrk.gif?account=urwtl1agWBr1N8" style="display:none" height="1" width="1" alt="" /></noscript>
-<!-- End Alexa Certify Javascript -->  
     
 </head>
 <body<?php echo $pg['body_id'] . $pg['body_class']; ?>>
