21f925f2d2b3b8ff174c20e033d7ccc37f86c24d
RobertClarke <robert@rjfc.net>
2015-09-12 21:18:19 -0500

    Update mods.php

--- mods.php
+++ mods.php
@@ -10,7 +10,7 @@ $pg = [
 	'title_main'	=> 'Mods',
 	'title_sub'		=> 'Minecraft PE',
 	'seo_desc'		=> 'Complete Minecraft PE mods make it easy to change the look and feel of your game. Updated often with the best mods for MCPE.',
-	'seo_keywords'	=> 'minecraft pe mods, minecraft pe, mcpe'
+	'seo_keywords'	=> 'minecraft pe mods, mcpe mods, minecraft pe, mcpe'
 ];
 
 show_header('Minecraft PE Mods', FALSE, $pg);
