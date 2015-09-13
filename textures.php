70eafddbeac3a8e8002d195e52e0d4670bc43b88
RobertClarke <robert@rjfc.net>
2015-09-12 21:22:12 -0500

    Update textures.php

--- textures.php
+++ textures.php
@@ -189,6 +189,6 @@ echo '
 
 <?php if ( $count != 0 ) $pagination->html(); ?>
 
-<p class="tracking">Tracking <?php echo number_format($count); ?> Texture Packs</p>
+<p class="tracking">Tracking <?php echo number_format($count); ?> MCPE Texture Packs</p>
 
 <?php show_footer(); ?>
