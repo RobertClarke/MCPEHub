ad0adf96b62a75127795879565ba29931e7c4e08
RobertClarke <robert@rjfc.net>
2016-04-21 17:47:07 -0700

    Update links.php

--- links.php
+++ links.php
@@ -19,12 +19,8 @@ show_header('Links', FALSE);
         NetherBox makes it easy to start your own Minecraft PC, or PE server.
     </p>
     <p>
-        <b><a href="http://instantmcpe.com/" target="_blank">Free Minecraft PE Hosting</a></b>
+        <b><a href="http://instantmcpe.com/" target="_blank">Free Minecraft Server Hosting</a></b>
         Start your own Minecraft PE server for free.
-    </p>
-    <p>
-        <b><a href="https://imc.io/" target="_blank">Free Minecraft Server Hosting</a></b>
-        Create a Minecraft PC server for free.
     </p>
     <p>
         <b><a href="http://minecraftcolorcodes.com/" target="_blank">Minecraft Color Codes</a></b>
