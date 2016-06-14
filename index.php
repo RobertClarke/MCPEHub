a4cebd4106442682ad6d92a9b1d2b0c8246598c1
Anatolie Diordita <contact@anatolie.me>
2016-06-13 21:15:45 -0400

    Fix SQL on homepage

--- index.php
+++ index.php
@@ -36,11 +36,15 @@ $db_posts = $db->query('
 	(SELECT "texture" 	AS type, '.$q_cols.' FROM `content_textures` WHERE '.$q_where.$q_end.') UNION ALL
 	(SELECT "skin" 		AS type, '.$q_cols.' FROM `content_skins` 	 WHERE '.$q_where.$q_end.') UNION ALL
 	(SELECT "mod" 		AS type, '.$q_cols.' FROM `content_mods` 	 WHERE '.$q_where.$q_end.') UNION ALL
-	(SELECT "server" 	AS type, '.$q_cols.' FROM `content_servers`  WHERE (id = 4) OR ('.$q_where.') ORDER BY featured_time DESC LIMIT 2)
+	(SELECT "server" 	AS type, '.$q_cols.' FROM `content_servers`  WHERE '.$q_where.$q_end.')
 
 	ORDER BY `featured_time` DESC
 
 ')->fetch();
+
+// Add sponsored server to top of server list
+$sponsored = $db->query('SELECT "server" AS type, '.$q_cols.' FROM `content_servers` WHERE id = 4 LIMIT 1');
+array_unshift($db_posts, $sponsored);
 
 // Grab additional info, organize posts array for use.
 foreach( $db_posts as $id => $post ) {
