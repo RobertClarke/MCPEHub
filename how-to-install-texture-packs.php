3299a02ffdb7cf361c07b940d822a1085a9f798f
RobertClarke <robert@rjfc.net>
2016-06-17 08:09:07 +0100

    Update how-to-install-texture-packs.php

--- how-to-install-texture-packs.php
+++ how-to-install-texture-packs.php
@@ -22,56 +22,59 @@ show_header('How To Install Minecraft PE Texture Packs', FALSE, $pg);
 
 <div id="how-to" class="tleft">
 	
-    <h2><i class="fa fa-apple fa-fw"></i> iOS Non-Jailbreak Install</h2>
+    <h2><i class="fa fa-apple fa-fw"></i>iOS Installation</h2>
     <ol class="spaced">
         <li>
-            Software you will need:<br />
-            - <a href="http://www.i-funbox.com/" target="_blank">iFunBox</a> or <a href="http://www.macroplant.com/iexplorer/" target="_blank">iExplorer</a><br />
-            - <a href="http://support.apple.com/downloads/#iphone" target="_blank">iPhone Configuration Utility</a><br />
-            - <a href="http://www.rarlab.com/download.htm" target="_blank">WinRAR</a> or any ZIP viewer
-        </li>
-        <li>Go to iTunes and get a version of Minecraft PE that you are using currently. Once you get it, drag it from the iTunes apps, to your desktop and it should be a .IPA file.</li>
-        <li>Now download a texture pack from our <a href="/textures" target="_blank">Texture Packs</a> list and extract it into a folder from a ZIP.</li>
-        <li>After that, drag and drop the .IPA into WinRAR. Once there, open "Payload" and then go to "minecraftpe.app".</li>
-        <li>Now copy and paste all the texture files you want into the "minecraftpe.app" and copy and replace all.</li>
-        <li>Close the program. If prompted, save all changes you've made.</li>
-        <li>Now get the iPhoneConfiguration Utility and install it. Drag the .IPA into the "Applications" top area in the running app.</li>
-        <li>An item called "minecraftpe" will appear with the update it will work with. Go into ‘Devices’ and go to the applications tab and uninstall "minecraftpe" and reinstall it.</li>
-        <li>Done! Open Minecraft PE and enjoy your texture pack!</li>
+			Hardware/software you will need:<br />
+			- Mac or Windows computer<br />
+			- iTunes for Mac or Windows<br />
+		</li>
+        <li>First, connect your device to your computer. </li>
+        <li>Open iTunes, and click on your device. </li>
+        <li>From the sidebar in iTunes, click <strong>Apps</strong>.</li>
+        <li>Scroll down on this page until you find the Minecraft: Pocket Edition app listed under "File Sharing". Click on the game. </li>
+        <li>To the right, click the <strong>games</strong> folder, then click <strong>Save to...</strong>, and choose your desired location to save this folder to. </li>
+        <li>Find and open the folder you just saved.</li>
+		<li>Inside of the folder, create a new one titled "resource_packs" (this title is case sensitive). </li>
+		<li>Open your new (resource_packs) folder, and drop in your extracted texture pack folder you downloaded earlier from <a href="/textures" target="_blank">our texture packs page</a>. </li>
+		<li>Head back over to iTunes and delete the original <strong>games</strong> folder from the Minecraft: Pocket Edition app. </li>
+		<li>Click the <strong>Add...</strong> button, navigate to the <strong>games</strong> you saved and made changes to earlier, and add this in. </li>
+		<li>Close iTunes and head back over to your mobile device. </li>
+		<li>Delete MC:PE from your mutlitasking page if you have not done so yet. </li>
+		<li>Open up your game, load a world, and have fun!</li>
     </ol>
-    <br>
-    <b>IMPORTANT: Make sure you have your device plugged in, and be sure to back up your maps, mods, etc. since it will return your Minecraft PE app to new since it was deleted and re-downloaded onto the device.</b>
     
     <div class="spacer"></div>
     
-    <h2><i class="fa fa-apple fa-fw"></i> iOS Jailbreak Install</h2>
+    <h2><i class="fa fa-apple fa-fw"></i> iOS Jailbreak Installation</h2>
     <ol>
         <li>
             Software you will need:<br />
-            - iFile (Found on Cydia)<br />
+            - iFile (found in Cydia)<br />
             - Safari Download Manager or Any Downloader<br />
         </li>
-        <li>First download a texture file from our <a href="/textures" target="_blank">Texture Packs</a> list with the downloader and choose to "Open w/ iFile".</li>
-        <li>Once in there choose extract all files/folders.</li>
-        <li>Hold down in the folder and choose "Select All". Then go to the bottom right, and choose Copy/Link.</li>
-        <li>Click the home button in the app and from there choose Applications and find the one labeled Minecraft PE. The from there go to "minecraftpe.app" and choose to paste all the files. Then delete the app from the multitasking menu.</li>
-        <li>Done! Open Minecraft PE and enjoy your texture pack!</li>
+        <li>First, download a texture pack file from our <a href="/textures" target="_blank">Texture Packs</a> list with the downloader, and choose to <strong>Open with iFile</strong> (or whichever downloader you have chosen to use for this process.</li>
+        <li>Once in there, choose <strong>Extract All Files/Folders</strong>.</li>
+        <li>Hold down in the folder and choose <strong>Select All</strong>; then go to the bottom right, and choose <strong>Copy/Link</strong>.</li>
+        <li>Click the home button in the app and from there choose the <strong>Applications</strong> folder and find the one labeled "Minecraft PE".</li>
+        <li>From there, go to <strong>minecraftpe.app</strong> and choose to paste all of the files. Then delete your downloader (and Minecraft: Pocket Edition if necessary) from the multitasking menu.</li>
+		<li>Open up your game, load a world, and have fun!</li>
     </ol>
     
     <div class="spacer"></div>
     
-    <h2><i class="fa fa-android fa-fw"></i> Android Unrooted Install</h2>
+    <h2><i class="fa fa-android fa-fw"></i> Android Installation</h2>
     <ol class="spaced">
         <li>
             Software you will need:<br />
             - Block Launcher<br />
             - Any File Manager or Astro File Manager<br />
         </li>
-        <li>Download a Minecraft PE texture pack from our <a href="/textures" target="_blank">Texture Packs</a> list.</li>
-        <li>Go to "Download" in the file manager and choose to extract files here.</li>
-        <li>Now go into Block Launcher and go to the Settings from the tool wrench.</li>
-        <li>Choose to "Use Texture Pack" and go to the Downloads and get the folder and click on the downloaded texture pack.</li>
-        <li>Done! Open Minecraft PE and enjoy your texture pack!</li>
+        <li>Download a texture pack file from our <a href="/textures" target="_blank">Texture Packs</a> list.</li>
+        <li>Go to <strong>Download</strong> in the file manager, and choose to extract files here.</li>
+        <li>Now open up Block Launcher and go to <strong>Settings</strong> from the tool wrench.</li>
+        <li>Choose to <strong>Use Texture Pack</strong>, and go to the <strong>Downloads</strong> and get the folder and click on the downloaded texture pack.</li>
+        <li>Open up your game, load a world, and have fun!</li>
     </ol>
 
 </div>
