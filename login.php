d1454ac778e322a61b7d9c62024240464026ab5a
Anatolie Diordita <contact@anatolie.me>
2016-12-22 01:28:14 -0500

    Disabled login/register pages.

--- login.php
+++ login.php
@@ -5,6 +5,25 @@
 **/
 
 require_once('core.php');
+
+// START PREVENT FUTURE LOGINS TEMP CODE
+show_header('Sign In', FALSE, ['body_id' => 'login', 'body_class' => 'boxed']);
+
+?>
+<div class="header"><h1>Sign In</h1></div>
+<div class="body">
+    <p style="text-align:center;">Login is currently unavailable due to routine maintenance. Please try again later.</p>
+</div>
+<div class="footer">
+    <div class="bttn-group">
+        <a href="/" class="bttn mid" style="width: 100%;">Back to Homepage</a>
+    </div>
+</div>
+<?php
+show_footer();
+die();
+// END PREVENT FUTURE LOGINS TEMP CODE
+
 if ( $user->logged_in() ) redirect('/dashboard');
 
 show_header('Sign In', FALSE, ['body_id' => 'login', 'body_class' => 'boxed']);
@@ -23,52 +42,52 @@ $f['redirect'] = isset( $_GET['redirect'] ) ? $_GET['redirect'] : NULL;
 $error->add('SUSPEND',	'Your account is currently suspended.');
 
 if ( !empty( $_POST ) ) {
-	
+
 	$error->reset();
-	
+
 	$error->add('MISSING',	'Both username &amp; password required.');
 	$error->add('INVALID',	'Incorrect username or password.');
-	
+
 	$f['username']	=	isset( $_POST['username'] ) ? $db->escape($_POST['username']) : NULL;
 	$f['password']	=	isset( $_POST['password'] ) ? $db->escape($_POST['password']) : NULL;
-	
+
 	$f['redirect']	=	isset( $_POST['redirect'] ) ? $_POST['redirect'] : $f['redirect'];
 	$f['remember']	=	isset( $_POST['remember'] ) ? TRUE : FALSE;
-	
+
 	// Check if username or password missing.
 	if ( empty($f['username']) || empty($f['password']) ) $error->set('MISSING');
 	else {
-		
+
 		// Check if username exists in database.
 		if ( !$user->check_username($f['username']) ) $error->set('INVALID');
 		else {
-		
+
 			// Verify password provided with database hash.
 			if ( !password_verify($f['password'], $user->info('password', $f['username']) ) ) $error->set('INVALID');
 			else {
-				
+
 				// Check if user suspended.
 				if ( $user->suspended($f['username']) ) $error->set('SUSPEND');
 				else {
-					
+
 					// Authenticate the browser.
 					$user->auth_set($f['username'], $f['remember']);
-					
+
 					// Update values in database.
 					$db->where(['username' => $f['username']])->update('users', ['last_ip' => current_ip(), 'last_login' => time_now()]);
-					
+
 					// Redirect user as needed.
 					if ( !empty($f['redirect']) ) redirect($f['redirect']);
 					else redirect('/dashboard?welcome');
-					
+
 				} // End: Check if user suspended.
-				
+
 			} // End: Verify password with database hash.
-			
+
 		} // End: Check if username exists in database.
-		
+
 	} // End: Check if username or password missing.
-	
+
 } // End: POST submission.
 
 if ( isset($_GET['suspended']) ) $error->force('SUSPEND');
--- register.php
+++ register.php
@@ -5,65 +5,84 @@
 **/
 
 require_once('core.php');
+
+// START PREVENT FUTURE LOGINS TEMP CODE
+show_header('Sign In', FALSE, ['body_id' => 'login', 'body_class' => 'boxed']);
+
+?>
+<div class="header"><h1>Create an Account</h1></div>
+<div class="body">
+    <p style="text-align:center;">Registration is currently unavailable due to routine maintenance. Please try again later.</p>
+</div>
+<div class="footer">
+    <div class="bttn-group">
+        <a href="/" class="bttn mid" style="width: 100%;">Back to Homepage</a>
+    </div>
+</div>
+<?php
+show_footer();
+die();
+// END PREVENT FUTURE LOGINS TEMP CODE
+
 if ( $user->logged_in() ) redirect('/dashboard');
 
 show_header('Register', FALSE, ['body_id' => 'register', 'body_class' => 'boxed wide']);
 
 if ( !empty($_POST) ) {
-	
+
 	$error->add('MISSING',		'You must fill in all the inputs in the form.');
-	
+
 	$error->add('U_LENGTH',		'Your username must be 5-20 characters long.');
 	$error->add('U_INVALID',	'Your username must start with a letter and contain "A-Z, 0-9, _" characters only.');
 	$error->add('U_USED',		'Your username is already in use by another member.');
-	
+
 	$error->add('E_LENGTH',		'Your email cannot be longer than 50 characters.');
 	$error->add('E_INVALID',	'Your email isn\'t in valid email format.');
 	$error->add('E_USED',		'Your email is already in use by another member.');
-	
+
 	$error->add('P_LENGTH',		'Your password must be 6-30 characters long.');
 	$error->add('P_USERNAME',	'Your password can\'t be the same as your username.');
 	$error->add('P_MATCH',		'Your passwords didn\'t match.');
-	
+
 	$f['username']		= isset( $_POST['username'] )	? $db->escape($_POST['username']) : NULL;
 	$f['email']			= isset( $_POST['email'] )		? $db->escape($_POST['email']) : NULL;
 	$f['password']		= isset( $_POST['password'] )	? $db->escape($_POST['password']) : NULL;
 	$f['password2']		= isset( $_POST['password2'] )	? $_POST['password2'] : NULL;
-	
+
 	// Check if any inputs missing.
 	if ( empty($f['username']) || empty($f['email']) || empty($f['password']) || empty($f['password2']) ) $error->set('MISSING');
 	else {
-		
+
 		// USERNAME field checks.
 		if ( !empty($f['username']) ) {
 			if		( !$form->length($f['username'], 20, 5) )	$error->append('U_LENGTH');
 			elseif	( !$form->alphanum($f['username']) )		$error->append('U_INVALID');
 			elseif	( $user->check_username($f['username']) )	$error->append('U_USED');
 		}
-		
+
 		// EMAIL field checks.
 		if ( !empty($f['email']) ) {
 			if		( !$form->length($f['email'], 50) )			$error->append('E_LENGTH');
 			elseif	( !is_email($f['email']) )					$error->append('E_INVALID');
 			elseif	( $user->check_email($f['email']) )			$error->append('E_USED');
 		}
-		
+
 		// PASSWORD field checks.
 		if ( !empty($f['password']) ) {
 			if		( !$form->length($f['password'], 30, 6) )	$error->append('P_LENGTH');
 			elseif	( $f['password'] == $f['username'] )		$error->append('P_USERNAME');
 			elseif	( $f['password'] != $f['password2'] )		$error->append('P_MATCH');
 		}
-		
+
 		if ( !$error->exists() ) {
-			
+
 			// Select a random avatar for user.
 			$avatar = ['cow', 'creeper', 'pig', 'skeleton', 'zombie', 'zombiepig'];
 			$avatar = 'default_'.$avatar[rand(0,5)].'.png';
-			
+
 			// Generate activation code.
 			$token = random_str(15);
-			
+
 			$u = [
 				'username'			=> $f['username'],
 				'password'			=> password_hash($f['password'], PASSWORD_DEFAULT),
@@ -73,10 +92,10 @@ if ( !empty($_POST) ) {
 				'avatar'			=> $avatar,
 				'activate_token'	=> $token
 			];
-			
+
 			// Insert user to database.
 			$db->insert('users', $u);
-			
+
 			// Send welcome email to user with activation instructions.
 			$e_body	 = "<p>Welcome to MCPE Hub! We can't wait to see what you have to contribute to our growing community, but first you need to activate your account.</p>\n";
 			$e_body .= "<p>Activate your account by clicking on <a href=\"".MAINURL."activate?code={$token}\">this link</a>.</p>\n";
@@ -84,16 +103,16 @@ if ( !empty($_POST) ) {
 			$e_body .= "<p>Thanks for joining our community!</p><p>- MCPE Hub Team</p>\n";
 			$e_body .= "<p><i>Follow us on Twitter: <a href=\"http://twitter.com/MCHubCommunity\">@MCHubCommunity</a> for news, updates, giveaways and more!</i></p>\n";
 			$e_body .= "<div class=\"bottom\">If you have issues opening the link above, use the following link: <a href=\"".MAINURL."activate?code={$token}\">".MAINURL."activate?code={$token}</a></div>\n";
-			
+
 			$email = $mail->format($e_body);
 			$email = $mail->send($f['email'], $f['username'], 'Activate Your Account', $email);
-			
+
 			redirect('/welcome');
-			
+
 		} // End: No errors found in inputs.
-		
+
 	} // End: Check if any inputs missing
-	
+
 } // End: POST submission.
 
 ?>
